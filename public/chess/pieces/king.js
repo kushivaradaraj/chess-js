76f718105c02ffdec8924b3dbaa1d7ad1910673f
kushivaradaraj <140957570+kushivaradaraj@users.noreply.github.com>
2024-09-26 02:15:45 +0530

    Update king.js

--- public/chess/pieces/king.js
+++ public/chess/pieces/king.js
@@ -1,11 +1,38 @@
-var King = function(config){
+var King = function(config) {
     this.type = 'king';
     this.constructor(config);
 };
 
-
-
 King.prototype = new Piece({});
-King.prototype.move = function(newPosition){
 
-}
+King.prototype.isValidPosition = function(targetPosition) {
+   let currentCol = this.position.charAt(0);
+   let currentRow = parseInt(this.position.charAt(1));
+   
+   
+   let targetCol = targetPosition.col.charAt(0);
+   let targetRow = parseInt(targetPosition.row);
+   
+
+   let colDifference = Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0));
+   let rowDifference = Math.abs(targetRow - currentRow);
+   
+   if ((colDifference === 1 && rowDifference === 0) || 
+       (colDifference === 0 && rowDifference === 1) || 
+       (colDifference === 1 && rowDifference === 1)) { 
+       return true; 
+   }
+   
+   return false;
+   
+   }
+
+
+King.prototype.moveTo = function(targetPosition) {
+    if (this.isValidPosition(targetPosition)) {
+        this.position = targetPosition.col + targetPosition.row;
+        this.render();  
+    } else {
+        console.warn("Move is not valid for king.");
+    }
+};
