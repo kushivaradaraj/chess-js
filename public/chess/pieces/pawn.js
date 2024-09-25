397261392329dfa05bcd232dfe055fd9f14c099d
kushivaradaraj <140957570+kushivaradaraj@users.noreply.github.com>
2024-09-26 02:19:23 +0530

    Update pawn.js

--- public/chess/pieces/pawn.js
+++ public/chess/pieces/pawn.js
@@ -3,10 +3,36 @@ var Pawn = function(config){
     this.constructor(config);
 };
 
-
-
 Pawn.prototype = new Piece({});
-Pawn.prototype.moveTo = function(targetPosition){
-    console.log(this);
-    console.log(targetPosition)
+
+Pawn.prototype.isValidPosition = function(targetPosition){
+    let currentCol = this.position.charAt(0);
+    let currentRow = parseInt(this.position.charAt(1));
+
+    let moveDistance = this.color === 'white' ? 1 : -1;
+    let initialRow = this.color === 'white' ? 2 : 7;
+
+    if (targetPosition.col === currentCol) {
+        if (targetPosition.row === (currentRow + moveDistance).toString()) {
+            return true;
+        } else if (currentRow === initialRow && targetPosition.row === (currentRow + 2 * moveDistance).toString()) {
+            return true;
+        }
+    } else if (Math.abs(targetPosition.col.charCodeAt(0) - currentCol.charCodeAt(0)) === 1 &&
+               targetPosition.row === (currentRow + moveDistance).toString()) {
+        return true;
+    }
+
+    console.warn("Invalid move for pawn");
+    return false;
+}
+
+Pawn.prototype.moveTo = function(targetPosition){    
+    if(this.isValidPosition(targetPosition)){
+        this.position = targetPosition.col + targetPosition.row;
+        this.render();
+    }else{
+        //NOOP
+    }
+    
 }
