42981f1eb44394b1a2684679674dff02d17dbfbe
Garvit Dadheech <147162890+GarvitDadheech@users.noreply.github.com>
2024-09-26 11:05:27 +0530

    Update king.js return values

--- public/chess/pieces/king.js
+++ public/chess/pieces/king.js
@@ -3,19 +3,14 @@ var King = function(config) {
     this.constructor(config);
 };
 
-// Inheriting from Piece
 King.prototype = new Piece({});
 
-// Implementing isValidPosition method for the King
 King.prototype.isValidPosition = function(targetPosition) {
-   // Convert current position to row and column
    let currentCol = this.position.charAt(0);
    let currentRow = parseInt(this.position.charAt(1));
-   
-   
+      
    let targetCol = targetPosition.col.charAt(0);
    let targetRow = parseInt(targetPosition.row);
-   
 
    let colDifference = Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0));
    let rowDifference = Math.abs(targetRow - currentRow);
@@ -37,8 +32,11 @@ King.prototype.moveTo = function(targetPosition) {
     if (this.isValidPosition(targetPosition)) {
         // If valid, update the king's position
         this.position = targetPosition.col + targetPosition.row;
-        this.render();  // Update the visual board
+        console.log(`King moved to ${this.position}`);
+        this.render();
+        return true;
     } else {
         console.warn("Move is not valid for king.");
+        return false;
     }
 };
